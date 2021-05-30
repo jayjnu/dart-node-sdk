42151a08aea4f6695c0211473d703981875a05c4
jayjnu <doublejnu@gmail.com>
2021-05-30 23:06:57 +0900

    build: setup husky

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = (api) => {
+  const isTest = api.env('test');
+
+  return {
+    "presets": [
+      "@babel/env",
+      "@babel/typescript"
+    ],
+    "plugins": [
+      "@babel/proposal-class-properties",
+      "@babel/proposal-object-rest-spread"
+    ],
+    "ignore": isTest ? [] : [
+      "**/__tests__",
+      "**/*.spec.ts"
+    ]
+  }
+};
