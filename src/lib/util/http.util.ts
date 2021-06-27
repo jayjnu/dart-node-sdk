80baaa601be93f6e75e2c3831431c163710ae112
jadestern <prelasia@gmail.com>
2021-06-27 13:03:56 +0900

    feat(api): remove unknown type

--- src/lib/util/http.util.ts
+++ src/lib/util/http.util.ts
@@ -15,16 +15,17 @@ export default class HttpClient2 {
   private http = Got;
   constructor(private url: string, private cert_key: string) {}
 
-  // TODO: generic 으로 unknown 을 없애고 싶은데 방법을 모르겠음 ResponseCommon 과 T 를 합성하고 싶음.
-  async getJSON(params: URLSearchParams): Promise<unknown> {
-    const json = (await this.http
+  async getJSON<T extends ResponseCommon>(
+    params: Record<string, unknown>
+  ): Promise<T> {
+    const json = await this.http
       .get(this.url, {
         searchParams: {
           cert_key: this.cert_key,
           ...params,
         },
       })
-      .json()) as ResponseCommon;
+      .json<T>();
 
     if (json.status !== '000') {
       throw new Error(json.message);
