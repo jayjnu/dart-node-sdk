55fefa35e320730638900f3853bf1cee06907e02
jadestern <prelasia@gmail.com>
2021-07-04 13:02:18 +0900

    feat(util): remove httpClient2 and update httpClient

--- src/lib/util/http.util.ts
+++ src/lib/util/http.util.ts
@@ -2,22 +2,19 @@ import got from 'got';
 import { ResponseCommon } from '../api/type';
 import { CancelableRequest, Response } from 'got/dist/source/as-promise';
 
-export const httpClient = got.extend({
-  prefixUrl: 'https://opendart.fss.or.kr/api',
-});
-
-export type HttpClient = typeof httpClient;
-
 const Got = got.extend({
   prefixUrl: 'https://opendart.fss.or.kr/api',
 });
 
-export default class HttpClient2 {
+export default class HttpClient {
   private http = Got;
-  constructor(private url: string, private cert_key: string) {}
+  constructor(private cert_key: string) {}
 
-  fetch(params: Record<string, unknown>): CancelableRequest<Response<string>> {
-    return this.http.get(this.url, {
+  fetch(
+    url: string,
+    params?: Record<string, unknown>
+  ): CancelableRequest<Response<string>> {
+    return this.http.get(url, {
       searchParams: {
         crtfc_key: this.cert_key,
         ...params,
@@ -26,16 +23,10 @@ export default class HttpClient2 {
   }
 
   async getJSON<T extends ResponseCommon>(
+    url: string,
     params: Record<string, unknown>
   ): Promise<T> {
-    const json = await this.http
-      .get(this.url, {
-        searchParams: {
-          crtfc_key: this.cert_key,
-          ...params,
-        },
-      })
-      .json<T>();
+    const json = await this.fetch(url, params).json<T>();
 
     if (json.status !== '000') {
       throw new Error(json.message);
