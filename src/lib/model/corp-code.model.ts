33e7b6adb79f9e8980ac8460c64990de5d103182
jayjnu <doublejnu@gmail.com>
2021-07-10 22:00:01 +0900

    test(model): write minimal specs for corp-code model
    corp code model should provide high level api of searching corp meta info by user query.
    Finding by
    company name is one of the essential features.
    
    ref #28

--- src/lib/model/corp-code.model.ts
+++ src/lib/model/corp-code.model.ts
@@ -1,5 +1,18 @@
-import type { CorpCodeAPI } from '~/api/corp-code.api';
+import type { Corp, CorpCodeAPI } from '~/api/corp-code.api';
+
+type CorpSearchOptions = {
+  exact?: boolean;
+};
 
 export class CorpCodeModel {
   constructor(private api: CorpCodeAPI) {}
+
+  // TODO: 구현 필요
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async getCorpByName(
+    name: string,
+    options?: CorpSearchOptions
+  ): Promise<Corp[]> {
+    return [];
+  }
 }
