6846afd2c44ae4e61f28f76a3c49a185fdeaa285
jayjnu <doublejnu@gmail.com>
2021-07-11 16:08:44 +0900

    test(util): write specs of HTTPInterceptor

--- src/lib/util/http-interceptor.util.spec.ts
+++ src/lib/util/http-interceptor.util.spec.ts
@@ -4,4 +4,7 @@ describe('HTTPInterceptor', () => {
   it('should pass', () => {
     expect(HTTPInterceptor).toBeDefined();
   });
+
+  it.todo('HttpInterceptor 구현체 리스트를 해당하는 hooks 배열에 추가한다.');
+  it.todo('HttpInterceptor 인터페이스에 해당하지 않는 hooks는 추가하지 않는다');
 });
