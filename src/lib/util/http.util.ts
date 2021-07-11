99594c2351efb81ee834e039a19152a79b84d472
jayjnu <doublejnu@gmail.com>
2021-07-11 15:45:15 +0900

    refactor(util): use extended got instance for searchParams

--- src/lib/util/http.util.ts
+++ src/lib/util/http.util.ts
@@ -1,24 +1,25 @@
-import got from 'got';
+import got, { Got } from 'got';
 import { ResponseCommon } from '../api/type';
 import { CancelableRequest, Response } from 'got/dist/source/as-promise';
 
-const Got = got.extend({
-  prefixUrl: 'https://opendart.fss.or.kr/api',
-});
-
 export default class HttpClient {
-  private http = Got;
-  constructor(private cert_key: string) {}
+  private http: Got;
+  constructor(private cert_key: string) {
+    this.http = got.extend({
+      prefixUrl: 'https://opendart.fss.or.kr/api',
+      searchParams: {
+        crtfc_key: this.cert_key,
+      },
+    });
+  }
 
   fetch(
     url: string,
-    params?: Record<string, unknown>
+    params?: Record<string, string | number>
   ): CancelableRequest<Response<string>> {
     return this.http.get(url, {
-      searchParams: {
-        crtfc_key: this.cert_key,
-        ...params,
-      },
+      searchParams: params,
+      hooks: {},
     });
   }
 
@@ -28,7 +29,7 @@ export default class HttpClient {
   ): Promise<Response> {
     const json = await this.fetch(
       url,
-      params as Record<string, unknown>
+      params as unknown as Record<string, string | number>
     ).json<Response>();
 
     if (json.status !== '000') {
